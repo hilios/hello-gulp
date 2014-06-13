b9745f8db2b9f44872461da2f88e976d6da31bcc
Edson Hilios <edson.hilios@telefonicabeta.com>
2014-06-13 11:31:06 -0300

    Build production files

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 var gulp = require('gulp');
 
 var less = require('gulp-less'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    uglify = require('gulp-uglify');
 
 gulp.task('build-css', function() {
   return gulp.src('less/bootstrap.less')
     .pipe(less())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(less({
+      compress: true
+    }))
+    .pipe(rename('bootstrap.min.css'))
     .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('build-js', function() {
   return gulp.src('js/**/*.js')
     .pipe(concat('bootstrap.js'))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(uglify())
+    .pipe(rename('bootstrap.min.js'))
     .pipe(gulp.dest('./dist/js'));
 });
 
