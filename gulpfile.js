95f539067e2e0d23e6e37a213cca375b7cce801a
Edson Hilios <edson.hilios@telefonicabeta.com>
2014-06-13 11:21:25 -0300

    Compile less and concat js

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,18 @@
 var gulp = require('gulp');
+
+var less = require('gulp-less'),
+    concat = require('gulp-concat');
+
+gulp.task('build-css', function() {
+  gulp.src('less/bootstrap.less')
+    .pipe(less())
+    .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('build-js', function() {
+  gulp.src('js/**/*.js')
+    .pipe(concat('bootstrap.js'))
+    .pipe(gulp.dest('./dist/js'));
+});
+
+gulp.task('build', ['build-js', 'build-css']);
